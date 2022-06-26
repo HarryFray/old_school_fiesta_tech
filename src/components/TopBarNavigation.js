e27c57d1cdb5694cee37fc0918ee72ce8a16b43e
nfray <harry.fray7@gmail.com>
2022-06-26 11:38:31 -0600

    are you sure log out modal

--- src/components/TopBarNavigation.js
+++ src/components/TopBarNavigation.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import { signOut } from "firebase/auth";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
+import ConfirmationModal from "../components/modal/Confirmation";
 import useWindowSize from "../hooks/useWindowSize";
 import Logo from "../images/Logo.ico";
 
@@ -57,6 +58,7 @@ const StyledTopBarNavigation = styled.div`
 `;
 
 const TopBarNavigation = ({ auth, currentUser }) => {
+  const [confirmationModalOpen, setConfirmationModalOpen] = useState(false);
   const navigate = useNavigate();
 
   const { isSmall } = useWindowSize();
@@ -68,51 +70,64 @@ const TopBarNavigation = ({ auth, currentUser }) => {
   };
 
   return (
-    <StyledTopBarNavigation>
-      {isSmall ? (
-        <div className="mobile_heading">
-          <div className="logo_and_text">
-            <img src={Logo} alt="Logo" width="32" height="32" />
-            <h2>Old Sol Fiesta</h2>
+    <>
+      <ConfirmationModal
+        confirmationModalModalOpen={confirmationModalOpen}
+        setConfirmationModalModalOpen={setConfirmationModalOpen}
+        confirmationAction={handleSignOut}
+        title="Are you sure you want to log out?"
+        text="Please don't leave me :("
+      />
+      <StyledTopBarNavigation>
+        {isSmall ? (
+          <div className="mobile_heading">
+            <div className="logo_and_text">
+              <img src={Logo} alt="Logo" width="32" height="32" />
+              <h2>Old Sol Fiesta</h2>
+            </div>
+            <h5>
+              {`What up ${
+                currentUser?.superUser ? "Super User" : currentUser?.displayName
+              }`}
+            </h5>
           </div>
-          <h5>
-            {`What up ${
-              currentUser?.superUser ? "Super User" : currentUser?.displayName
-            }`}
-          </h5>
-        </div>
-      ) : (
-        <>
-          <div className="logo_and_text">
-            <img src={Logo} alt="Logo" width="40" height="40" />
-            <h1>Old Sol Fiesta</h1>
-          </div>
-          <h4>
-            {`What up ${
-              currentUser?.superUser ? "SuperUser" : currentUser?.displayName
-            }`}
-          </h4>
-        </>
-      )}
-      <div className="navigation">
-        {currentUser?.superUser && (
+        ) : (
           <>
-            <h3>
-              <Link to="/lottery">Lottery</Link>
-            </h3>
-            <h3>
-              <Link to="/events">Event Management</Link>
-            </h3>
-            <h3>
-              <Link to="/dashboard">Dashboard</Link>
-            </h3>
+            <div className="logo_and_text">
+              <img src={Logo} alt="Logo" width="40" height="40" />
+              <h1>Old Sol Fiesta</h1>
+            </div>
+            <h4>
+              {`What up ${
+                currentUser?.superUser ? "SuperUser" : currentUser?.displayName
+              }`}
+            </h4>
           </>
         )}
-        <Button onClick={handleSignOut} variant="outlined" size="small">
-          Sign Out
-        </Button>
-      </div>
-    </StyledTopBarNavigation>
+        <div className="navigation">
+          {currentUser?.superUser && (
+            <>
+              <h3>
+                <Link to="/lottery">Lottery</Link>
+              </h3>
+              <h3>
+                <Link to="/events">Event Management</Link>
+              </h3>
+              <h3>
+                <Link to="/dashboard">Dashboard</Link>
+              </h3>
+            </>
+          )}
+          <Button
+            onClick={() => setConfirmationModalOpen(true)}
+            variant="outlined"
+            size="small"
+          >
+            Sign Out
+          </Button>
+        </div>
+      </StyledTopBarNavigation>
+    </>
   );
 };
 
