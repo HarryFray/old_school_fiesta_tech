ca4a85dae1eda87546fec943f6513fca5bb29bf2
nfray <harry.fray7@gmail.com>
2022-06-26 13:07:39 -0600

    progress gloabal snack back

--- src/global/Layout.js
+++ src/global/Layout.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
 
 import TopBarNavigation from "../components/TopBarNavigation";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const DEFAULT_SNACKBAR = {
+  open: false,
+  message: "",
+  status: "success",
+};
 
 const StyledLayout = styled.div`
   width: 100vw;
@@ -20,11 +32,24 @@ const StyledLayout = styled.div`
 `;
 
 const Layout = ({ children, auth, currentUser }) => {
+  const [snackBarConfig, setSnackBarConfig] = useState(DEFAULT_SNACKBAR);
+
   return (
-    <StyledLayout>
-      <TopBarNavigation auth={auth} currentUser={currentUser} />
-      <div className="page_content">{children}</div>
-    </StyledLayout>
+    <>
+      <Snackbar
+        open={snackBarConfig?.open}
+        autoHideDuration={3000}
+        onClose={() => setSnackBarConfig("")}
+      >
+        <Alert severity={snackBarConfig?.status} sx={{ width: "100%" }}>
+          {snackBarConfig?.message}
+        </Alert>
+      </Snackbar>
+      <StyledLayout>
+        <TopBarNavigation auth={auth} currentUser={currentUser} />
+        <div className="page_content">{children}</div>
+      </StyledLayout>
+    </>
   );
 };
 
